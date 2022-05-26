4664d822bba1248bb36baf39321469658f8c02ee
Aghs <agiospantas@gmail.com>
2022-05-27 01:29:27 +0300

    Updates

--- src/Components/ChatTab.js
+++
@@ -1,8 +0,0 @@
-export default function ChatTab(props) {
-  return (
-    <div className="ChatTabContainer">
-      chat container
-      <div className="ChatTabInner">chat inner</div>
-    </div>
-  );
-}
